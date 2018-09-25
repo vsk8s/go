2f8540cb13b0a38a4f7eebfcc558b5848d21ecb3
Michael Hudson-Doyle <michael.hudson@canonical.com>
2018-09-26 09:01:43 +1200

    d/helpers/getver.pl: small improvements, thanks Guillem Jover. (Closes: #909604)

--- debian/helpers/getver.pl
+++ debian/helpers/getver.pl
@@ -2,13 +2,13 @@
 
 # Extract upstream version from version in changelog.
 
-package main;
-
 use warnings;
 use strict;
 
-use Dpkg::Version;
+use Dpkg::Changelog::Parse;
 
-my $v = Dpkg::Version->new(`dpkg-parsechangelog -SVersion`)->version();
+my $fields = changelog_parse();
+my $v = $fields->{Version}->version();
 $v =~ /^([0-9]+\.[0-9]+).*/ or die 'INVALID VERSION! (needs X.Y)';
+
 printf("%s\n", $1);
